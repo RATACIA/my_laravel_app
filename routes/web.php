da79de99988d6f01fab311e6bbaef4a752bd75c6
RATACIA <134484195+RATACIA@users.noreply.github.com>
2024-10-16 22:17:01 +0300

    testing routes

--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,40 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    $person = [
-        'name'=> 'Zura',
-        'email'=> 'zura@example.com',
-    ];
-    dd($person);
+   
+    $productUrl = route("product.view", ["lang"=> 'en', "id"=>123]);
+    dd($productUrl);
     return view('welcome');
 });
-Route::view('/about', 'about');
+Route::view('/about-us', 'about')->name('about');
+
 
 Route::get("/product/{id}", function(string $id) {
-return "product id = $id";
+return "Works! $id";
+}) -> whereNumber("id");
+
+
+Route::get("/user/{username}", function(string $username) {
+    return "Works! $username";
+})->where("username","[a-z]+");
+
+
+Route::get("{lang}/product/{id}", function (string $lang, string $id) {
+    return "The lang is $lang with id of $id";
+})->where([
+    'lang' => '[a-z]{2}',
+    'id' => '\d{4,}' 
+])->name("product.view");
+
+
+Route::get("/search/{search}", function(string $search) {
+    return "$search";
+})->where("search", ".+");
+
+Route::get("/user/profile", function() {
+})->name('profile'); 
+
+Route::get('/current-user', function() {
+    // return redirect()->route('profile');
+    return to_route('profile');
 });
